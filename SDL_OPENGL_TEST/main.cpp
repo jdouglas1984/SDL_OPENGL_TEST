ee3f7fc74df3d6c8c2330907c6baf1898368238f
jdouglas1984 <mstangfrk@gmail.com>
2015-01-26 12:13:24 -0600

    Initial Commit

---
+++ SDL_OPENGL_TEST/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SDL_OPENGL_TEST
+//
+//  Created by Josh on 1/26/15.
+//  Copyright (c) 2015 Josh. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
