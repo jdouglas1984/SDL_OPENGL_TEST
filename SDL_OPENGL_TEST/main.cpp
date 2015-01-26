c68b18b038b7a972b2d8d227f5eaf2cac3991415
jdouglas1984 <mstangfrk@gmail.com>
2015-01-26 13:06:58 -0600

    Setup of SDL2

--- SDL_OPENGL_TEST/main.cpp
+++ SDL_OPENGL_TEST/main.cpp
@@ -7,9 +7,58 @@
 //
 
 #include <iostream>
+#include <SDL2/SDL.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+
+int main(int argc, const char * argv[])
+{
+    int width = 800;
+    int height = 600;
+    
+    if(0 != SDL_Init(SDL_INIT_EVERYTHING))
+    {
+        cout << "Could not initialize SDL: " <<  SDL_GetError() << endl;
+        return 0;
+    }
+    
+    SDL_LogSetAllPriority(SDL_LOG_PRIORITY_VERBOSE);
+    
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+    
+    auto window = SDL_CreateWindow("SDL OpenGL test", 0, 0, width, height, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
+    
+    if(window == 0)
+    {
+        SDL_Quit();
+        return 0;
+    }
+    
+    SDL_Event event;
+    
+    bool done = false;
+    
+    while (!done)
+    {
+        SDL_PumpEvents();
+        while (SDL_PollEvent(&event))
+        {
+            switch (event.type)
+            {
+                case SDL_QUIT:
+                    done = true;
+                    break;
+                case SDL_APP_LOWMEMORY:
+                    cout << "Low Memory" << endl;
+                    break;
+            }
+        }
+    }
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 }
